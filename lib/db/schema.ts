d6f8667bd33fc596e3be4679e385f42f6ab42970
rahulkpr2510 <rahulkpr2510@gmail.com>
2026-01-26 20:59:20 +0530

    feat: Set up Drizzle ORM with Neon HTTP, defining a files table schema and migration scripts.

---
+++ lib/db/schema.ts
@@ -0,0 +1,35 @@
+import { pgTable, text, uuid, boolean, integer, timestamp } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+
+export const files = pgTable("files", {
+    id: uuid("id").defaultRandom().primaryKey(),
+
+    name: text("name").notNull(),
+    path: text("path").notNull(), // /document/project/file.pdf
+    size: integer("size").notNull(),
+    type: text("type").notNull(), // pdf, doc, image, folder
+
+    fileUrl: text("file_url").notNull(),
+    thumbnailUrl: text("thumbnail_url"),
+
+    userId: text("user_id").notNull(),
+    parentId: uuid("parent_id"), // parent folder id (null in case of root folder)
+
+    isFolder: boolean("is_folder").default(false).notNull(),
+    isStarred: boolean("is_starred").default(false).notNull(),
+    isTrash: boolean("is_trash").default(false).notNull(),
+
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
+})
+
+export const filesRelations = relations(files, ({ one, many }) => ({
+    parent: one(files, {
+        fields: [files.parentId],
+        references: [files.id],
+    }),
+    children: many(files)
+}))
+
+export const File = typeof files.$inferSelect;
+export const NewFile = typeof files.$inferInsert;
